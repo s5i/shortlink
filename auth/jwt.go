62aa71e881afb382c709273597167809100792a4
Wojciech Szymski <wojciechszymski@gmail.com>
2022-06-30 22:00:49 +0200

    Add auth library.

---
+++ auth/jwt.go
@@ -0,0 +1,61 @@
+package auth
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+func (a *Auth) jwtVerify(token string) (string, bool) {
+	t, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return a.jwtSecret, nil
+	})
+	if err != nil {
+		return "", false
+	}
+
+	if claims, ok := t.Claims.(jwt.MapClaims); ok {
+		if !t.Valid {
+			return "", false
+		}
+		email, ok := claims["email"].(string)
+		if !ok {
+			return "", false
+		}
+		return email, true
+	}
+	return "", false
+
+}
+
+func (a *Auth) jwtBuild(email string) (string, time.Time, error) {
+	now := time.Now()
+	deadline := now.Add(a.jwtTTL)
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email": email,
+		"iat":   now.Unix(),
+		"exp":   deadline.Unix(),
+	})
+
+	token, err := t.SignedString(a.jwtSecret)
+	if err != nil {
+		return "", time.Time{}, err
+	}
+	return token, deadline, nil
+}
+
+func (a *Auth) jwtSetCookie(w http.ResponseWriter, name, email string) error {
+	jwt, expires, err := a.jwtBuild(email)
+	if err != nil {
+		return errors.New("failed to build JWT")
+	}
+
+	http.SetCookie(w, &http.Cookie{Name: name, Value: jwt, Path: "/", Expires: expires})
+	return nil
+}
---
+++ auth/oauth.go
@@ -0,0 +1,84 @@
+package auth
+
+import (
+	"crypto/rand"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"golang.org/x/oauth2"
+)
+
+type oauthState struct {
+	previousPath string
+}
+
+func (a *Auth) oAuthDialog(w http.ResponseWriter, r *http.Request) {
+	state := randomString(32)
+
+	url := a.oAuthCfg.AuthCodeURL(state)
+	a.oAuthInFlight[state] = &oauthState{previousPath: r.URL.Path}
+
+	http.Redirect(w, r, url, http.StatusFound)
+}
+
+func (a *Auth) oAuthCallback(w http.ResponseWriter, r *http.Request) {
+	states := r.URL.Query()["state"]
+	if len(states) != 1 {
+		http.Error(w, "need exactly one state in URL params", http.StatusBadRequest)
+		return
+	}
+	state, ok := a.oAuthInFlight[states[0]]
+	if !ok {
+		http.Error(w, "bad CSRF token", http.StatusForbidden)
+		return
+	}
+
+	codes := r.URL.Query()["code"]
+	if len(codes) != 1 {
+		http.Error(w, "need exactly one code in URL params", http.StatusBadRequest)
+		return
+	}
+
+	tok, err := a.oAuthCfg.Exchange(oauth2.NoContext, codes[0])
+	if err != nil {
+		http.Error(w, "failed to exchange code for token", http.StatusForbidden)
+		return
+	}
+
+	client := a.oAuthCfg.Client(oauth2.NoContext, tok)
+	resp, err := client.Get("https://www.googleapis.com/oauth2/v1/userinfo")
+	defer resp.Body.Close()
+
+	dec := json.NewDecoder(resp.Body)
+	var authInfo struct {
+		Email    string `json:"email"`
+		Verified bool   `json:"verified_email"`
+	}
+	if err := dec.Decode(&authInfo); err != nil {
+		http.Error(w, "failed to decode JSON response", http.StatusInternalServerError)
+		return
+	}
+	if !authInfo.Verified {
+		http.Error(w, "Google returned verified_email = False", http.StatusForbidden)
+		return
+	}
+
+	if err := a.jwtSetCookie(w, a.jwtCookieName, authInfo.Email); err != nil {
+		http.Error(w, "failed to build JWT", http.StatusInternalServerError)
+		return
+	}
+
+	if state.previousPath != "" {
+		http.Redirect(w, r, state.previousPath, http.StatusFound)
+		return
+	}
+
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func randomString(len int) string {
+	b := make([]byte, len)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)[:len]
+}
